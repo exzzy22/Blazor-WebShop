944a5389bedb8258c57fe900669c785ff48f3b4f
Borna Hilc <borna.hilc@protonmail.com>
2022-12-03 13:54:57 +0100

    Update create product page to use validations attributes only

--- ViewModels/ProductForCreationVM.cs
+++ ViewModels/ProductForCreationVM.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Immutable;
 using System.ComponentModel.DataAnnotations;
+using System.Data.SqlTypes;
 using ViewModels.Static;
 using ViewModels.ValidationAttributes;
 
@@ -9,18 +10,31 @@ public class ProductForCreationVM
 {
     [Required]
     public string Name { get; set; } = null!;
+
     [Required]
     public int? Discount { get; set; }
+
     [Required]
     public int? InStock { get; set; }
+
     [Required]
     public int? CategoryId { get; set; }
-    public AttributesVM Attributes { get; set; } = null!;
+
+    [Required]
+    public AttributesVM? Attributes { get; set; } = null!;
+
     [CollectionCount<PriceForCreationVM>("Product must contain at least one price", 1)]
     public List<PriceForCreationVM> Prices { get; set; } = new();
+
+    [ProductImages]
     public ICollection<PictureForCreationVM> Pictures { get; set; } = null!;
+
     public ImmutableList<string> Specifications { get; } = AttributesDictionary.Attributes.ToImmutableList();
+
     [Required]
     public string? SelectedSpecification { get; set; }
+
+    [Range(typeof(bool), "true", "true")]
+    public bool IsFormValid { get; set; } = false;
 
 }
---
+++ ViewModels/ValidationAttributes/ProductImages.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ViewModels.ValidationAttributes;
+
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+public class ProductImages : ValidationAttribute
+{
+    string errorMessage = "";
+    protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+    {
+        IEnumerable<PictureForCreationVM> list = value as IEnumerable<PictureForCreationVM>;
+
+        if (list is null || list.Count() < 1 || !list.Any(i => i.MainImage))
+        {
+            if (list is null || list.Count() < 1)
+                errorMessage = "Image is required";
+
+            else if(!list.Any(i => i.MainImage))
+                errorMessage = "Set one image as main image";
+
+            return new ValidationResult(errorMessage, new List<string> { validationContext.MemberName ?? "" });
+        }
+
+        return ValidationResult.Success;
+    }
+}
