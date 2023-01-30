974b8fe03a67c3875fc1b4ae3955379356219015
Borna Hilc <borna.hilc@protonmail.com>
2023-01-30 22:43:42 +0100

    Added pagination

--- Shared/RequestFeatures/ProductPagedList.cs
+++ Shared/RequestFeatures/ProductPagedList.cs
@@ -31,4 +31,23 @@ public class ProductPagedList<T> : PagedList<T>
 
 		return new ProductPagedList<T>(items, count, pageNumber, pageSize, categoryCount, manufacturerCount);
 	}
+
+	public new List<int> GetPagesToShow()
+	{
+		int start = MetaData.CurrentPage - (5 / 2);
+		int end = MetaData.CurrentPage + (5 / 2);
+		if (start < 1)
+		{
+			end += 1 - start;
+			start = 1;
+		}
+		if (end > MetaData.TotalPages)
+		{
+			start -= end - MetaData.TotalPages;
+			end = MetaData.TotalPages;
+		}
+		if (start < 1) start = 1;
+		if (end > MetaData.TotalPages) end = MetaData.TotalPages;
+		return Enumerable.Range(start, end - start + 1).ToList();
+	}
 }
